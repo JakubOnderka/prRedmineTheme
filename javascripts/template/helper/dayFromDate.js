acacec8dab1996e1ff5c4b669e284d482537e796
Jakub Onderka <jakub.onderka@gmail.com>
2015-02-28 12:14:20 +0100

    Fix dayFromDate

--- javascripts/template/helper/dayFromDate.js
+++ javascripts/template/helper/dayFromDate.js
@@ -1,5 +1,5 @@
 define(['vendor/handlebars.runtime'], function (handlebars) {
   handlebars.registerHelper('dayFromDate', function(key) {
-    return key.split('-')[2];
+    return parseInt(key.split('-')[2]);
   });
 });
