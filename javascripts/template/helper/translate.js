034428719c3e2f5d895c53a0b88daf2043044b9e
Jakub Onderka <jakub.onderka@gmail.com>
2015-03-12 22:41:37 +0100

    Support only for numeral arguments in translate Handlebars helper

--- javascripts/template/helper/translate.js
+++ javascripts/template/helper/translate.js
@@ -1,12 +1,9 @@
 define(['vendor/handlebars.runtime', 'lib/translate'], function (handlebars, _) {
-  handlebars.registerHelper('_', function(key, options) {
+  handlebars.registerHelper('_', function(key) {
     var translated = _(key);
 
-    var i = 0;
-    for (var a in options.data.root) {
-      var value = options.data.root[a];
-      translated = translated.replace('%' + (i++), value);
-      translated = translated.replace('%' + a, value);
+    for (var i = 1; i < (arguments.length - 1); i++) {
+      translated = translated.replace('%' + (i - 1), arguments[i]);
     }
 
     return translated;
