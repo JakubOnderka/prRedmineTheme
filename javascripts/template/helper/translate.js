1382400de97a8544cccd656bb4b06787bad51f1f
Jakub Onderka <jakub.onderka@gmail.com>
2015-03-12 22:30:39 +0100

    Fixed translate Handlebars helper

--- javascripts/template/helper/translate.js
+++ javascripts/template/helper/translate.js
@@ -1,11 +1,12 @@
 define(['vendor/handlebars.runtime', 'lib/translate'], function (handlebars, _) {
-  handlebars.registerHelper('_', function(key) {
+  handlebars.registerHelper('_', function(key, options) {
     var translated = _(key);
 
     var i = 0;
-    for (var a in this) {
-      translated = translated.replace('%' + (i++), this[a]);
-      translated = translated.replace('%' + a, this[a]);
+    for (var a in options.data.root) {
+      var value = options.data.root[a];
+      translated = translated.replace('%' + (i++), value);
+      translated = translated.replace('%' + a, value);
     }
 
     return translated;
