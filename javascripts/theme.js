35c427d399c5fbe7311341a26401bc1e7eb209c4
Jakub Onderka <jakub.onderka@gmail.com>
2015-08-19 09:33:31 +0200

    Fixed close icon for Timey logger

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -5077,7 +5077,7 @@ templates['relations_header'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main
 templates['timey_loger'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper;
 
-  return "<div class=\"timeyLoggerWrapper\">\n    <span class=\"close\"><i class=\"glyphicon glypicon-remove\"></i></span>\n    <iframe style=\"border:0; width: 100%; height: 220px\" src=\""
+  return "<div class=\"timeyLoggerWrapper\">\n    <span class=\"close\"><span class=\"glyphicon glyphicon-remove\"></span></span>\n    <iframe style=\"border:0; width: 100%; height: 220px\" src=\""
     + this.escapeExpression(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"url","hash":{},"data":data}) : helper)))
     + "\"></iframe>\n</div>";
 },"useData":true});
