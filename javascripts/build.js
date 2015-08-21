fe1f1da66a7cb805b93fc832280d60266cc92af6
Jakub Onderka <jakub.onderka@gmail.com>
2015-08-21 09:14:09 +0200

    Experimental usage of Almond instead of full require.js and minize output script

--- javascripts/build.js
+++ javascripts/build.js
@@ -1,6 +1,9 @@
 ({
   baseUrl: ".",
   name: "main",
-  out: "tmp.js",
-  optimize: "none"
+  include: ["vendor/almond.js"],
+  out: "theme.js",
+  optimize: "uglify2",
+  generateSourceMaps: true,
+  preserveLicenseComments: false
 })
--- javascripts/build.sh
+++ javascripts/build.sh
@@ -6,12 +6,7 @@ handlebars -a -h "vendor/" template/*.handlebars -f templates.js
 # Build require.js
 node r.js -o build.js
 
-rm theme.js
-cat vendor/require.js > theme.js
-cat tmp.js >> theme.js
-
 # Hack: undefined 'define.amd' for working datepicker
 echo "\ndefine.amd = undefined;" >> theme.js
 
-rm tmp.js
 rm templates.js
