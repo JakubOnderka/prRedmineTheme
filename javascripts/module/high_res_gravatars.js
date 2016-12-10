19ed3ff613a7bc8c7a41ca162c6bc2e7cd536895
Jakub Onderka <ahoj@jakubonderka.cz>
2016-12-10 12:30:23 +0100

    'Size=' in gravatar image URL is optional

--- javascripts/module/high_res_gravatars.js
+++ javascripts/module/high_res_gravatars.js
@@ -11,20 +11,24 @@ define({
       $('.gravatar').each(function () {
         var img = this;
 
-        img.src = img.src.replace(/size=([0-9]*)(x([0-9]*))?/, function (match, p1, p2, p3) {
+        img.src = img.src.replace(/(size=)?([0-9]*)(x([0-9]*))?/, function (match, size, p1, p2, p3) {
+          if (typeof size === 'undefined') {
+            size = '';
+          }
+
           // People module
           if (p3) {
             img.width = p1;
             img.height = p3;
 
-            return 'size=' + (p1 * 2) + 'x' + (p3 * 2);
+            return size + (p1 * 2) + 'x' + (p3 * 2);
 
           // Gravatar
           } else {
             img.width = p1;
             img.height = p1;
 
-            return 'size=' + (p1 * 2);
+            return size + (p1 * 2);
           }
         });
       })
